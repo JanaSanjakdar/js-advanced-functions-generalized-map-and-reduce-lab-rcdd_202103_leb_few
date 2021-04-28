ca03e62d4b925c050b05e674ca64a3783d2d55b5
jana sanjakdar <jana.sanjakdar@hotmail.com>
2021-04-28 20:29:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // Add your functions here
 function map (srcArray, fctn){
   let arr =[];
+  for (let i=0; i<srcArray.length; i++){
+    let stg = srcArray[i]
+    arr.push(fctn(stg))
+  } 
+  return arr;
 }
