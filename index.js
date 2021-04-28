ebd1df93d522e9de88e1be8485e3ba493d78d652
jana sanjakdar <jana.sanjakdar@hotmail.com>
2021-04-28 20:36:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ function map (srcArray, fctn){
   for (let i=0; i<srcArray.length; i++){
     let stg = srcArray[i]
     arr.push(fctn(stg))
-  }
+  } 
   return arr;
+}
+
+function reduce (srcArray,fctn, starting){
+  let ray = (!!start) ? start : srcArray[0]
+  let i = (!!start) ? 0: 1
+
+  for (; i < array.length; i++) {
+  
+  } 
+    
+  }
 }
